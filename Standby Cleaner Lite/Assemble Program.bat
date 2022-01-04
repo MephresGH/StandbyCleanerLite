0ef8894a40824c9286e69c6e7843109823b29abd
Mephres <58438219+MephresGH@users.noreply.github.com>
2022-01-04 15:13:48 +0100

    Add files via upload
    Repository required to build program natively with .NET 3.5

---
+++ Standby Cleaner Lite/Assemble Program.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d %~dp0
+c:\windows\Microsoft.NET\Framework\v3.5\csc.exe /t:exe /out:"Standby Cleaner Lite.exe" "Program.cs" "AssemblyInfo.cs" /win32icon:StandbyCleanerLite.ico
+pause
---
+++ Standby Cleaner Lite/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("A lightweight 'Empty Standby List' alternative.")]
+[assembly: AssemblyDescription("Standby Cleaner Lite")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mephres")]
+[assembly: AssemblyProduct("Standby Cleaner Lite")]
+[assembly: AssemblyCopyright("Copyright © 2021 Mephres")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a68ea91f-7980-4084-a7db-f86ed79e40c9")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
