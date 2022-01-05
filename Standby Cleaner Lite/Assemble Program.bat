021ed106dfed9d3185c622119ece16a6ba41685c
Mephres <58438219+MephresGH@users.noreply.github.com>
2022-01-05 18:38:30 +0100

    Add files via upload

--- Standby Cleaner Lite/Assemble Program.bat
+++ Standby Cleaner Lite/Assemble Program.bat
@@ -1,4 +1,7 @@
 @echo off
 cd /d %~dp0
+mkdir bin
 c:\windows\Microsoft.NET\Framework\v3.5\csc.exe /t:exe /out:"Standby Cleaner Lite.exe" "Program.cs" "AssemblyInfo.cs" /win32icon:"StandbyCleanerLite.ico"
+move "Standby Cleaner Lite.exe" "bin"
+explorer "%~dp0bin"
 pause
