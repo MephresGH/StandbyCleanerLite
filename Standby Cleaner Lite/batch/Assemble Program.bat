954701f6f3c4eb8802b4743a17e6479387a625a9
Mephres <58438219+MephresGH@users.noreply.github.com>
2022-01-05 22:02:21 +0100

    Add files via upload

---
+++ Standby Cleaner Lite/batch/Assemble Program.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "%~dp0.."
+mkdir "%~dp0..\bin"
+c:\windows\Microsoft.NET\Framework\v3.5\csc.exe /t:exe /out:"Standby Cleaner Lite.exe" "Program.cs" "AssemblyInfo.cs" /win32icon:"StandbyCleanerLite.ico"
+move "Standby Cleaner Lite.exe" "bin"
+explorer "%~dp0..\bin"
+pause
---
+++ Standby Cleaner Lite/batch/Create Task.bat
@@ -0,0 +1,25 @@
+@echo off
+GOTO :CHECKPERMS
+
+:CHECKPERMS
+    echo Administrative Permissions Required. Detecting Permissions...
+
+    NET SESSION >nul 2>&1
+    IF %errorLevel% == 0 (
+        echo Success: Administrative Permissions Confirmed. Continuing As Usual... && GOTO :PROMPT
+    ) ELSE (
+        echo Current Permissions Inadequate. Requesting Elevated Rights... && (powershell start -verb runas '%0' am_admin & exit /b)
+    )
+
+:PROMPT
+CLS
+SCHTASKS /CREATE /TN "Standby Cleaner Lite" /TR "%~dp0..\bin\Standby Cleaner Lite.exe" /SC MINUTE /MO 5 /RU SYSTEM /RL HIGHEST /F >NUL 2>&1
+SCHTASKS /QUERY /TN "Standby Cleaner Lite" /XML > "%~dp0..\bin\StandbyCleanerLite.xml"
+cd /d "%~dp0..\bin"
+powershell -Command "(gc 'StandbyCleanerLite.xml' -raw) -replace '<Settings>', '<Settings> <Hidden>true</Hidden>' | Set-Content 'StandbyCleanerLite.xml'"
+SCHTASKS /END /TN "Standby Cleaner Lite" >NUL 2>&1
+SCHTASKS /DELETE /TN "Standby Cleaner Lite" /F >NUL 2>&1
+SCHTASKS /CREATE /XML "StandbyCleanerLite.xml" /TN "Standby Cleaner Lite"
+SCHTASKS /RUN /TN "Standby Cleaner Lite"
+DEL "StandbyCleanerLite.xml" /F >NUL 2>&1
+pause
