952a96ee4fe16e37d1f7a1cbb0da7f38c28ccbe1
Mephres <58438219+MephresGH@users.noreply.github.com>
2022-01-12 02:03:16 +0100

    Add files via upload

--- Standby Cleaner Lite/batch/Create Task.bat
+++ Standby Cleaner Lite/batch/Create Task.bat
@@ -6,13 +6,20 @@ GOTO :CHECKPERMS
 
     NET SESSION >nul 2>&1
     IF %errorLevel% == 0 (
-        echo Success: Administrative Permissions Confirmed. Continuing As Usual... && GOTO :PROMPT
+        echo Success: Administrative Permissions Confirmed. Continuing As Usual... && GOTO :FILECHECK
     ) ELSE (
         echo Current Permissions Inadequate. Requesting Elevated Rights... && (powershell start -verb runas '%0' am_admin & exit /b)
     )
 
-:PROMPT
+:FILECHECK
 CLS
+IF EXIST "%~dp0..\bin\Standby Cleaner Lite.exe" GOTO :PROMPT (
+) ELSE (
+echo ERROR: no required files found. Closing terminal... && timeout /t 3
+taskkill /f /im cmd.exe
+)
+
+:PROMPT
 SCHTASKS /CREATE /TN "Standby Cleaner Lite" /TR "%~dp0..\bin\Standby Cleaner Lite.exe" /SC MINUTE /MO 5 /RU SYSTEM /RL HIGHEST /F >NUL 2>&1
 SCHTASKS /QUERY /TN "Standby Cleaner Lite" /XML > "%~dp0..\bin\StandbyCleanerLite.xml"
 cd /d "%~dp0..\bin"
